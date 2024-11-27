7874e8024e4affcac58923dc04aadef9e0c883c0
misterG420 <65085030+misterG420@users.noreply.github.com>
2024-11-27 11:25:52 +0000

    outro level

--- Assets/Scripts/DialogueManagerOutroLevel.cs
+++ Assets/Scripts/DialogueManagerOutroLevel.cs
@@ -1,20 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DialogueManagerOutroLevel : MonoBehaviour
 {
-    [SerializeField] private Text dialogueText; 
-    [SerializeField] private Button goodSidebutton1;    
-    [SerializeField] private Button darkSidebutton2;    
-    [SerializeField] private string[] dialogueLines; 
+    [SerializeField] private Text dialogueText;
+    [SerializeField] private Button goodSidebutton1;
+    [SerializeField] private Button darkSidebutton2;
+    [SerializeField] private string[] dialogueLines;
 
+    [SerializeField] private float typingSpeed = 0.05f; 
     private int currentLineIndex = 0;
 
     private void Start()
     {
-
         goodSidebutton1.gameObject.SetActive(false);
         darkSidebutton2.gameObject.SetActive(false);
 
@@ -28,13 +27,23 @@ public class DialogueManagerOutroLevel : MonoBehaviour
     {
         while (currentLineIndex < dialogueLines.Length)
         {
-            dialogueText.text = dialogueLines[currentLineIndex];
+            yield return StartCoroutine(TypeLine(dialogueLines[currentLineIndex]));
             currentLineIndex++;
             yield return new WaitForSeconds(4f); 
         }
 
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(4f); 
         goodSidebutton1.gameObject.SetActive(true);
         darkSidebutton2.gameObject.SetActive(true);
+    }
+
+    private IEnumerator TypeLine(string line)
+    {
+        dialogueText.text = ""; 
+        foreach (char letter in line.ToCharArray())
+        {
+            dialogueText.text += letter; 
+            yield return new WaitForSeconds(typingSpeed); 
+        }
     }
 }
