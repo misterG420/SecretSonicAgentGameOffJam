f7a7391bdaae41cb60b675a61d05305f3d521187
misterG420 <65085030+misterG420@users.noreply.github.com>
2024-11-27 14:04:20 +0000

    small scale anim

--- Assets/Scripts/RandomScaling.cs
+++ Assets/Scripts/RandomScaling.cs
@@ -1,18 +1,42 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomScaling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private Vector2 scaleRange = new Vector2(0.9f, 1.1f);
+    [SerializeField] private Vector2 durationRange = new Vector2(1f, 3f);
+
+
+    private Vector3 originalScale;
+
+    private void Start()
     {
-        
+        originalScale = transform.localScale;
+        StartCoroutine(ScaleRandomly());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ScaleRandomly()
     {
-        
+        while (true)
+        {
+
+            float targetScale = Random.Range(scaleRange.x, scaleRange.y);
+            float duration = Random.Range(durationRange.x, durationRange.y);
+
+            Vector3 targetVector = originalScale * targetScale;
+            Vector3 startScale = transform.localScale;
+
+            float elapsedTime = 0f;
+            while (elapsedTime < duration)
+            {
+                elapsedTime += Time.deltaTime;
+                transform.localScale = Vector3.Lerp(startScale, targetVector, elapsedTime / duration);
+                yield return null;
+            }
+
+
+            yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+        }
     }
 }
